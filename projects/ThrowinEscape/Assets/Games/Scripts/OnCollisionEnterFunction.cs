8af8182cdb860947be32f2609afd0842d26b4842
Ryoma-h777 <chocolate.rh19@ezweb.ne.jp>
2017-01-21 18:18:43 +0900

    Create OnCollisionEnterFunction

---
+++ projects/ThrowinEscape/Assets/Games/Scripts/OnCollisionEnterFunction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEnterFunction : MonoBehaviour {
+	[Tooltip("objectに物が当たった場合に、このObjを消滅するか")]
+	/// <summary>
+	/// このobjectに何か物が当たったら、このObjは消滅するかどうか
+	/// </summary>
+	public bool isDestroy;
+
+	[Tooltip("tagを設定した場合、tagと一致したときのみ当たったことにします。ｓ")]
+	/// <summary>
+	/// 設定したTagしか反応しなくなります。
+	/// </summary>
+	public string hitTag = "";
+
+	void OnCollisionEnter(Collider other)
+	{
+		Debug.Log("hit other Tag: " + other.tag);
+		if(hitTag == "" || hitTag == other.tag)
+		{
+			// ここでHit数をAddしてください
+			Debug.Log("hit!");
+
+			//すぐ消えるので、Animetion後に消す場合は消してはいけません。
+			if( isDestroy )
+			{
+				Destroy(this);
+			}
+		}
+	}
+}
