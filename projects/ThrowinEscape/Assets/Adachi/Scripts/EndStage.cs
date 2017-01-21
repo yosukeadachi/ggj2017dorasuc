ffb3aa00948fc1e47879ba52fd1e35c5615cf92d
yosukeadachi <yosukeadachi@gmail.com>
2017-01-21 14:55:01 +0900

    シーン遷移コントロール追加 エンディング判定はまだ

---
+++ projects/ThrowinEscape/Assets/Adachi/Scripts/EndStage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndStage : MonoBehaviour {
+
+	public ConfirmStage titleCsObj;
+	float timeCounter = 0;
+	public const float TIME_OUT_SECONDS = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		timeCounter = 0;	//memo: ０リセットトリガーは別でもたないとだめかも
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isTimeOver()) {
+			StageResultManager.FaileStage(gameObject.name);
+			titleCsObj.Confirm();
+		}
+		else if(isSuccessEscape()) {
+			StageResultManager.SuccessStage(gameObject.name);
+			titleCsObj.Confirm();
+		}
+	}
+
+	//時間オーバー
+	bool isTimeOver() {
+		return (timeCounter > TIME_OUT_SECONDS);
+	}
+
+	//脱出成功
+	//@TODO ここを変更してください
+	bool isSuccessEscape() {
+		return false;
+	}
+}
---
+++ projects/ThrowinEscape/Assets/Adachi/Scripts/StageResultManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageResultManager : MonoBehaviour {
+
+	static Dictionary<string, bool> stageResults = new Dictionary<string, bool> () {
+		{"Stage1", false},
+		{"Stage2", false}
+	};
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//ステージ成功
+	public static void SuccessStage(string aStageName) {
+		setStageResult(aStageName, true);
+	}
+
+	//ステージ失敗
+	public static void FaileStage(string aStageName) {
+		setStageResult(aStageName, false);
+	}
+
+	//ステージクリア情報保存
+	static void setStageResult(string aStageName , bool aIsSuccess) {
+		if(stageResults.ContainsKey(aStageName)) {
+			stageResults[aStageName] = aIsSuccess;
+		}
+	}
+		
+}
