c329bb8da963444549842a6d9b9093c1111b8ca8
Ryoma-h777 <chocolate.rh19@ezweb.ne.jp>
2017-01-21 22:10:46 +0900

    create title prefab

---
+++ projects/ThrowinEscape/Assets/Games/Scripts/CheckRingCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckRingCollider : MonoBehaviour {
+
+	bool m_isHit = false;
+	public bool IsHit { get { return m_isHit; } }
+
+	public string m_hitTag;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		Debug.Log(other.gameObject.tag);
+		if (m_hitTag == other.gameObject.tag)
+		{
+			ConfirmStage confirm = other.gameObject.GetComponent<ConfirmStage>();
+			if (confirm == null)
+			{
+				Debug.LogError("投げたものにConfirmがついてないよ！");
+				return;
+			}
+			m_isHit = true;
+		}
+	}
+
+}
