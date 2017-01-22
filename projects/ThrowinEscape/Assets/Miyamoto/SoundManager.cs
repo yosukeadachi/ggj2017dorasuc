31f447dbbe79f886c5a705d963bc5546f940da8c
TSITCL\s142177 <zgsann@gmail.com>
2017-01-22 14:12:17 +0900

    音を再生するスプライトを追加

---
+++ projects/ThrowinEscape/Assets/Miyamoto/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    // 音
+    public AudioClip sound;
+    AudioSource m_audio;
+
+	// Use this for initialization
+	void Start () {
+        m_audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void PlaySoundOneShot()
+    {
+        m_audio.PlayOneShot(sound);
+    }
+}
