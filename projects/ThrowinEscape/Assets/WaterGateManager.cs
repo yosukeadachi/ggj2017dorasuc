36ca6cded37144fa43597aaf76b133abf86f974d
Ryoma-h777 <chocolate.rh19@ezweb.ne.jp>
2017-01-22 13:28:26 +0900

    StageBalloon add WaterGate

---
+++ projects/ThrowinEscape/Assets/WaterGateManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGateManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos3 = transform.position;
+		newPos3.y += 0.005f;
+		transform.position = newPos3;
+	}
+}
